22846f27e55f595ab6dc0529a0379018bf9bab16
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-28 15:40:47 -0400

    first

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,21 @@
 def char_count(str):
   # Your code here
+  new_str = "".join(sorted(str.replace(" ","")))
+
+  letters = {}
+
+  for x in new_str:
+    if x in letters:
+      letters[x] += 1
+    else:
+      letters.update({x:1})
+  
+  answer = []
+  for j,i in letters.items():
+    answer.append([j] + [i])
+  answer = sorted(answer, key = lambda x:x[1], reverse = True)
+
+
+  return answer
+
+  
